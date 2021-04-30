130e6d38737043f2e8d3810833505340befd8c73
pranavmalvawala <pranavmalvawala@gmail.com>
2021-04-30 15:30:52 +0530

    Update author, siteUrls and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://pranavmalvawala.com',
       },
     },
     'gatsby-plugin-typescript',
