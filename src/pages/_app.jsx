875082242ecaf6788a96edc59c601cda9b2e2732
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-09-15 20:06:55 +0530

    update bg

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -20,19 +20,12 @@ export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
 
   return (
-    <>
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div>
-      <div className="relative">
-        <Header />
-        <main>
-          <Component previousPathname={previousPathname} {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div className="bg-white dark:bg-zinc-900">
+      <Header />
+      <main>
+        <Component previousPathname={previousPathname} {...pageProps} />
+      </main>
+      <Footer />
+    </div>
   )
 }
