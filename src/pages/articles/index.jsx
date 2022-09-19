e47e743b7b347c2da95122b7d342e3244ef25a6f
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-09-19 22:19:06 +0530

    add restricting folder access blog

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,15 +38,15 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - Spencer Sharp</title>
+        <title>Articles - Pranav Malvawala</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="Posts, tutorials, snippets, notes, and everything else, collected in chronological order."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on software, tools, code and more..."
+        intro="Posts, tutorials, snippets, notes, and everything else, collected in chronological order."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
